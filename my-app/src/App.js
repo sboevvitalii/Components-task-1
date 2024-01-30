40e0a8eed4ccaf5d1516f9eca32f491c4a9c73ff
sboev.vitalii <sboev.vitalii@mail.ru>
2024-01-30 16:06:35 +0600

    added 0 at the start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,94 +7,94 @@ const NUMBER = "NUMBER";
 const OPERATOR = "OPERATOR";
 
 export const App = () => {
-	const [operand1, setOperand1] = useState("");
-	const [operand2, setOperand2] = useState("");
-	const [operator, setOperator] = useState("");
-	const [isResult, setIsResult] = useState(false);
+  const [operand1, setOperand1] = useState("");
+  const [operand2, setOperand2] = useState("");
+  const [operator, setOperator] = useState("");
+  const [isResult, setIsResult] = useState(false);
 
-	const clear = () => {
-		setOperand1("");
-		setOperand2("");
-		setOperator("");
-		setIsResult(false);
-	};
+  const clear = () => {
+    setOperand1("");
+    setOperand2("");
+    setOperator("");
+    setIsResult(false);
+  };
 
-	const handleClick = (value, type) => () => {
-		if (type === NUMBER) {
-			if (operand1 && operator) {
-				setOperand2((prev) => prev + value);
-			} else {
-				setOperand1((prev) => prev + value);
-			}
-		}
+  const handleClick = (value, type) => () => {
+    if (type === NUMBER) {
+      if (operand1 && operator) {
+        setOperand2((prev) => prev + value);
+      } else {
+        setOperand1((prev) => prev + value);
+      }
+    }
 
-		if (type === OPERATOR) {
-			switch (value) {
-				case "C":
-					clear();
-					break;
-				case "=":
-					setIsResult(true);
-					break;
-				default:
-					if (operand1) {
-						setOperator(value);
-					}
-			}
-		}
+    if (type === OPERATOR) {
+      switch (value) {
+        case "C":
+          clear();
+          break;
+        case "=":
+          setIsResult(true);
+          break;
+        default:
+          if (operand1) {
+            setOperator(value);
+          }
+      }
+    }
 
-		if (isResult) {
-			clear();
-		}
-	};
+    if (isResult) {
+      clear();
+    }
+  };
 
-	const renderOutput = () => {
-		let output = "";
+  const renderOutput = () => {
+    let output = "0";
 
-		if (isResult) {
-			if (operator === "+") {
-				output = Number(operand1) + Number(operand2);
-			}
+    if (isResult) {
+      if (operator === "+") {
+        output = Number(operand1) + Number(operand2);
+      }
 
-			if (operator === "-") {
-				output = Number(operand1) - Number(operand2);
-			}
-		} else {
-			output = `${operand1}${operator}${operand2}`;
-		}
+      if (operator === "-") {
+        output = Number(operand1) - Number(operand2);
+      }
+    } else {
+      output = `${operand1 || 0}${operator}${operand2}`;
+    }
 
-		return output;
-	};
+    return output;
+  };
 
-	return (
-		<div className={styles.app}>
-			<div className={styles.container}>
-				<div className={`${styles.display} ${isResult ? styles.outcome : ""}`}>
-					{renderOutput()}
-				</div>
-				<div className={styles.operators}>
-					{OPERATORS.map((item) => (
-						<button
-							className={styles.operand}
-							key={item}
-							onClick={handleClick(item, OPERATOR)}
-						>
-							{item}
-						</button>
-					))}
-				</div>
-				<div className={styles.nums}>
-					{NUMS.map((item) => (
-						<button
-							key={item}
-							className={styles.button}
-							onClick={handleClick(item, NUMBER)}
-						>
-							{item}
-						</button>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.app}>
+      <div className={styles.container}>
+        <div className={`${styles.display} ${isResult ? styles.outcome : ""}`}>
+          {renderOutput()}
+        </div>
+        <div className={styles.operators}>
+          {OPERATORS.map((item) => (
+            <button
+              className={styles.operand}
+              key={item}
+              onClick={handleClick(item, OPERATOR)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+        <div className={styles.nums}>
+          {NUMS.map((item) => (
+            <button
+              key={item}
+              className={styles.button}
+              onClick={handleClick(item, NUMBER)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
